d6d17fec37523040f4e81a016970cc53786734d2
Mario Pavlov <mario.g.pavlov@gmail.com>
2020-09-01 16:12:43 +0300

    Add license text.

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# ScriptPack consumer - example project
-Clone this repo and run:
+# ScriptPack consumer
+
+This is a fully functional example project accompanying the [ScriptPack](https://github.com/SoftwareAG/scriptpack) project.
+
+To try the example clone this repo and run:
+
 ```
 gradle clean build copyCurrentJvmToPayload copyLibsToPayload zipPayload generateScriptPack
 ```
+
 Then go to build/distributions and run:
+
 ```
 scriptpack -d C:\myapp --internal-skip-privilege-check
 ```
 
+***
+
+This code is provided as-is and without warranty or support. It does not constitute part of the Software AG product suite. Users are free to use, fork and modify it, subject to the license agreement. While Software AG welcomes contributions, we cannot guarantee to include every contribution in the master project.
--- launcher.bat
+++ launcher.bat
@@ -1,13 +1,11 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-REM Initialization.
 set WORKDIR=%~dp0
 set WORKDIR=%WORKDIR:~0,-1%
 set DEFAULT_APP_HOME=%WORKDIR%
 set APP_HOME=%DEFAULT_APP_HOME%
 
-REM Handling command line parameters.
 if -%1-==-- set SHOW_HELP=1
 for %%i in (%*) do (
   if defined INSTALL_DIR set APP_HOME=%%~i& set INSTALL_DIR=
@@ -24,24 +22,11 @@ if defined SHOW_HELP (
     echo.
     echo Options:
     echo        -h, --h,--help            This usage help.
-    echo        -d C:\install\dir         Target installation directory.
-    echo                                  (default: %DEFAULT_APP_HOME%^)
-REM echo        --internal-skip-privilege-check
-REM echo                                  Skipping Administrator privileges check
-REM echo                                  is internal and not documented.
+    echo        -d C:\myapp               Target installation directory.
     echo.
     exit /b 0
 )
 
-if not defined SKIP_PRIV_CHECK (
-    REM An alternative is to use something like: whoami /priv | findstr SeCreateGlobalPrivilege | findstr Enabled
-    net session > NUL 2>&1 || (
-        echo Administrator privileges required to execute bootstrap. Please run Command Prompt as Administrator and try again.
-        exit /b 1
-    )
-)
-
-REM Remove trailing slash
 if %APP_HOME:~-1%==\ set APP_HOME=%APP_HOME:~0,-1%
 
 md "%APP_HOME%" 2> NUL
